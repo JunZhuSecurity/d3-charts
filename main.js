5ee6bdf81f6049223a95fc88ee5aab8687a98f96
Christian Rodemeyer <atombrenner@gmail.com>
2014-03-16 17:41:13 +0100

    using some real cpu data

--- main.js
+++ main.js
@@ -1,99 +1,85 @@
-
-function convert(d) {
-    var result = {};
-    d3.keys(d).forEach(function(key) {
-        result[key] = (key !== 'Time') ? +d[key] : Date.parse(d[key]);
-    });
-    return result;
-}
-
-time = [
-"2014-02-28 16:18:51 +0100",
-"2014-02-28 16:23:51 +0100",
-"2014-02-28 16:48:56 +0100",
-"2014-02-28 17:18:58 +0100",
-"2014-02-28 17:49:01 +0100",
-"2014-02-28 18:18:45 +0100",
-"2014-02-28 18:48:44 +0100",
-"2014-02-28 19:19:03 +0100",
-"2014-02-28 19:49:03 +0100",
-"2014-02-28 20:19:00 +0100",
-"2014-02-28 20:48:59 +0100",
-"2014-02-28 21:18:59 +0100",
-"2014-02-28 21:48:58 +0100",
-"2014-02-28 22:18:56 +0100"];
-
-time.forEach(function(t, i, a){a[i] = Date.parse(t);});
-
-
 (function(){
-    var ydata = [0,0,0,1,2,4,2,2,6,1,5,5,3,4];
-    var ydata2 = [4,5,9,7,6,7,8,2,4,6,7,8,9,7];
 
     var margin = {top: 20, right: 30, bottom: 30, left: 40};
-    var width = 960 - margin.left - margin.right;
-    var height = 500 - margin.top - margin.bottom;
+    var width = 1024 - margin.left - margin.right;
+    var height = 400 - margin.top - margin.bottom;
 
-    var scaleX = d3.time.scale()
-        .domain([time[0], time[time.length - 1]])
-        .range([0, width]);
-    var scaleY = d3.scale.linear()
-        .domain([0,10])
-        .range([height, 0]);
+    function createAxis(chart, scaleX, scaleY) {
+        var xAxis = d3.svg.axis()
+            .scale(scaleX)
+       //     .tickFormat(d3.time.format("%H:%M"))
+            .orient("bottom");
 
-    var chart = d3.select("#chart")
-        .append('svg:svg')
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        chart.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
 
-    var xAxis = d3.svg.axis()
-        .scale(scaleX)
-        .tickFormat(d3.time.format("%H:%M"))
-        .orient("bottom");
+        var yAxis = d3.svg.axis().scale(scaleY).orient("left");
 
-    chart.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        chart.append("g").attr("class", "y axis").call(yAxis)
+            .append("text").text("Used #CPU").attr("class", "label")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 16)
+          //  .attr("x", -height / 2)
+            .style("text-anchor", "end");
+    }
 
-    var yAxis = d3.svg.axis().scale(scaleY).orient("left");
+    function createChart(data) {
+        var scaleX = d3.time.scale()
+            .domain([data.time[0], data.time[data.time.length - 1]])
+            .range([0, width]);
+        var scaleY = d3.scale.linear()
+            .domain([0,8])
+            .range([height, 0]);
 
-    chart.append("g").attr("class", "y axis").call(yAxis)
-         .append("text").text("Milliseconds (ms)")
-         .attr("transform", "rotate(-90)")
-         .attr("y", -margin.left + 13)
-         .attr("x", -height / 2)
-         .style("text-anchor", "middle");
+        var chart = d3.select("#chart")
+            .append('svg:svg')
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var line = d3.svg.line()
-        .interpolate("basis")
-        .x(function(d, i) {
-            return scaleX(time[i]); })
-        .y(function(d) {
-            return scaleY(d); });
+        createAxis(chart, scaleX, scaleY);
 
-    chart.append("path")
-         .datum(ydata)
-         .attr("class", "line")
-         .attr("d", line);
+        var line = d3.svg.line()
+            .interpolate("basis")
+            .x(function(d, i) {
+                return scaleX(data.time[i]); })
+            .y(function(d) {
+                return scaleY(d); });
 
-    scatter = chart.append("g").attr("transform","translate(-2, -2)");
+        chart.append("path")
+             .datum(data.cpu)
+             .attr("class", "cpu-line")
+             .attr("d", line);
 
-    scatter.selectAll(".cpu").data(ydata).enter().append("rect")
-         .attr("class", "cpu")
-         .attr("x", function(d, i) {return scaleX(time[i]);})
-         .attr("y", function(d) {return scaleY(d);})
-         .attr("width", 5)
-         .attr("height", 5);
+    }
 
-    scatter.selectAll(".cpu2").data(ydata2).enter().append("rect")
-         .attr("class", "cpu2")
-         .attr("x", function(d, i) {return scaleX(time[i]);})
-         .attr("y", function(d) {return scaleY(d);})
-         .attr("width", 5)
-         .attr("height", 5);
+    function scatter(data) {
+        // i'm using 5x5 rects as data point
+        scatter = chart.append("g").attr("transform","translate(-2, -2)");
 
+      scatter.selectAll(".cpu").data(ydata).enter().append("rect")
+           .attr("class", "cpu")
+           .attr("x", function(d, i) {return scaleX(data.time[i]);})
+           .attr("y", function(d) {return scaleY(d);})
+           .attr("width", 5)
+           .attr("height", 5);
+
+      scatter.selectAll(".cpu2").data(ydata2).enter().append("rect")
+           .attr("class", "cpu2")
+           .attr("x", function(d, i) {return scaleX(time[i]);})
+           .attr("y", function(d) {return scaleY(d);})
+           .attr("width", 5)
+           .attr("height", 5);
+    }
+
+
+    // Main
+    d3.json("data/timeline.json", function(error, json) {
+        json.time.forEach(function(t, i, a){a[i] = Date.parse(t) + 3600000;}); // 0100 Offset
+        createChart(json);
+    });
 
 })();
--- prepare.rb
+++ prepare.rb
@@ -1,2 +1,33 @@
 require 'csv'
+require 'json'
+
+TIME = 0
+CPU = 26 # data[0].find_index('CPU') #note that the TCP header was forgotten
+
+result = []
+
+(1..6).each do |i|
+  data = CSV.read("monitoring/lcdnscv00#{i}.csv").drop(1)
+  data.each_index do |row|
+    if data[row][CPU]
+      if i == 1
+        result[row] = [data[row][TIME], data[row][CPU].to_f]
+      else
+        result[row][i] = data[row][CPU].to_f
+      end
+    end
+  end
+end
+
+result = result.reduce([]){|memo, row| row.nil? ? memo : (memo << row)}
+
+cpu_per_vm = 2
+json = {}
+
+json[:cpu] = result.map{|row| row.drop(1).reduce(0){|memo, cpu| memo + cpu_per_vm * (cpu ? cpu : 0) / 100}.round(2)}
+json[:time] = result.map{|row| row[0].sub(/ \+0100$/, '')}
+
+File.write('data/timeline.json', json.to_json)
+
+puts json.to_json
 
