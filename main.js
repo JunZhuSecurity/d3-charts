4b848fa7fc312fa82c659e82384b2cf16af9a6ab
Christian Rodemeyer <atombrenner@gmail.com>
2014-03-24 20:34:45 +0100

    using marker

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
     var margin = {top: 20, right: 30, bottom: 30, left: 40};
     var width = 1024 - margin.left - margin.right;
-    var height = 400 - margin.top - margin.bottom;
+    var height = 300 - margin.top - margin.bottom;
 
     function createAxis(chart, scaleX, scaleY) {
         var xAxis = d3.svg.axis()
@@ -26,14 +26,14 @@
     }
 
     function createChart(data) {
-        var scaleX = d3.time.scale()
+        var scaleX = d3.time.scale.utc()
             .domain([data.time[0], data.time[data.time.length - 1]])
             .range([0, width]);
         var scaleY = d3.scale.linear()
-            .domain([0,8])
+            .domain([0,7])
             .range([height, 0]);
 
-        var chart = d3.select("#chart")
+        var chart = d3.select("#cpu")
             .append('svg:svg')
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
@@ -43,17 +43,78 @@
         createAxis(chart, scaleX, scaleY);
 
         var line = d3.svg.line()
-            .interpolate("basis")
+            //.interpolate("basis")
             .x(function(d, i) {
                 return scaleX(data.time[i]); })
             .y(function(d) {
                 return scaleY(d); });
 
         chart.append("path")
-             .datum(data.cpu)
              .attr("class", "cpu-line")
+             .datum(data.cpu)
              .attr("d", line);
 
+
+        var release = Date.parse("2014-03-17T00:00:00");
+        chart.append("line")
+            .attr("class", "marker-line")
+            .attr("x1", scaleX(release))
+            .attr("x2", scaleX(release))
+            .attr("y1", 0)
+            .attr("y2", height);
+    }
+
+    function createMemoryChart(data) {
+
+        var width = 1024 - margin.left - margin.right;
+        var height = 200 - margin.top - margin.bottom;
+
+        var scaleX = d3.time.scale()
+            .domain([data.time[0], data.time[data.time.length - 1]])
+            .range([0, width]);
+        var scaleY = d3.scale.linear()
+            .domain([0,4000])
+            .range([height, 0]);
+
+        var chart = d3.select("#ram")
+            .append('svg:svg')
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var xAxis = d3.svg.axis()
+            .scale(scaleX)
+            .orient("bottom");
+
+        chart.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        var yAxis = d3.svg.axis().scale(scaleY).orient("left");
+
+        chart.append("g").attr("class", "y axis").call(yAxis)
+            .append("text").text("Available Memory [MB]").attr("class", "label")
+            .attr("x", -150)
+            .attr("transform", "rotate(-90)")
+            .attr("y", 16)
+            .style("text-anchor", "end");
+
+        var line = d3.svg.line()
+            .interpolate("basis")
+            .x(function(d, i) {
+                return scaleX(data.time[i]); })
+            .y(function(d) {
+                return scaleY(d); });
+
+        data.ram[0] = data.ram[3];
+        data.ram[1] = data.ram[4];
+        data.ram[2] = data.ram[5];
+        chart.append("path")
+            .datum(data.ram)
+            .attr("class", "ram-line")
+            .attr("d", line);
     }
 
     function scatter(data) {
@@ -78,8 +139,10 @@
 
     // Main
     d3.json("data/timeline.json", function(error, json) {
-        json.time.forEach(function(t, i, a){a[i] = Date.parse(t) + 3600000;}); // 0100 Offset
+        var format = d3.time.format("%Y-%m-%d %H:%M:%S");
+        json.time.forEach(function(t, i, a){a[i] = +format.parse(t) + 3600000;}); // 0100 Offset
         createChart(json);
+        createMemoryChart(json);
     });
 
 })();
--- prepare.rb
+++ prepare.rb
@@ -3,6 +3,7 @@ require 'json'
 
 TIME = 0
 CPU = 26 # data[0].find_index('CPU') #note that the TCP header was forgotten
+RAM = 27
 
 result = []
 
@@ -11,10 +12,19 @@ result = []
   data.each_index do |row|
     if data[row][CPU]
       if i == 1
-        result[row] = [data[row][TIME], data[row][CPU].to_f]
-      else
-        result[row][i] = data[row][CPU].to_f
+        result[row] = [data[row][TIME], [], [] ]
       end
+      result[row][1] << data[row][CPU].to_f
+      result[row][2] << data[row][RAM].to_f
+    end
+  end
+end
+(1..3).each do |i|
+  data = CSV.read("monitoring/lcdnsck00#{i}.csv").drop(1)
+  data.each_index do |row|
+    if data[row][CPU]
+      result[row][1] << data[row][CPU].to_f
+      result[row][2] << data[row][RAM].to_f
     end
   end
 end
@@ -24,8 +34,9 @@ result = result.reduce([]){|memo, row| row.nil? ? memo : (memo << row)}
 cpu_per_vm = 2
 json = {}
 
-json[:cpu] = result.map{|row| row.drop(1).reduce(0){|memo, cpu| memo + cpu_per_vm * (cpu ? cpu : 0) / 100}.round(2)}
 json[:time] = result.map{|row| row[0].sub(/ \+0100$/, '')}
+json[:cpu] = result.map{|row| row[1].reduce(0){|memo, cpu| memo + cpu_per_vm * (cpu ? cpu : 0) / 100}.round(2)}
+json[:ram] = result.map{|row| (row[2].reduce(0){|memo, ram| memo + ram} / row[2].length).round(2)}
 
 File.write('data/timeline.json', json.to_json)
 
