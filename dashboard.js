027f5eca2f69072d19588bdddc18113e797904e4
Christian Rodemeyer <atombrenner@gmail.com>
2014-03-30 19:21:35 +0200

    more styling

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,7 @@
 d3.json("dashboard.json", function(error, json) {
 
     function kpiHtml(title, value) {
-        return "<div class='title'>" + title + "</div><div class='usage'>" + value + "</div>";
+        return "<div class='title'>" + title + "</div><div class='value'>" + value + "</div>";
     }
 
     var groups = d3.select("#Groups").selectAll("div.row").data(json).enter();
@@ -18,12 +18,23 @@ d3.json("dashboard.json", function(error, json) {
     //appendKPI()
 
     row.append("div").attr("class", "kpi").attr("title", "Peak usage last 10 days").html(function(d){
-        return kpiHtml("45% CPU", "234");
+        var title = Math.round(d.cpu.used / d.cpu.total * 100) + "% CPU";
+        var value = d.cpu.used + "/" + d.cpu.total;
+        return kpiHtml(title, value);
     });
     row.append("div").attr("class", "kpi-space");
     row.append("div").attr("class", "kpi").html(function(d){
-        return kpiHtml("45% CPU", "234");
-    })
+        var title = Math.round(d.ram.used / d.ram.total * 100) + "% RAM [GiB]";
+        var value = d.ram.used + "/" + d.ram.total;
+        return kpiHtml(title, value);
+    });
+
+    row = kpis.append("div").attr("class", "kpi-row");
+    row.append("div").attr("class", "kpi").attr("title", "Disk").html(function(d){
+           var title = Math.round(d.cpu.used / d.cpu.total * 100) + "% CPU";
+           var value = d.cpu.used + "/" + d.cpu.total;
+           return kpiHtml(title, value);
+       });
 
 });
 
