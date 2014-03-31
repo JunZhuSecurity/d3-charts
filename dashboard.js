a8714750c3561708142f46a4a81ddfedf9ef161a
Christian Rodemeyer <crodemeyer@autoscout24.de>
2014-03-31 14:09:19 +0200

    generating kpis

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,28 @@
 d3.json("dashboard.json", function(error, json) {
 
-    function kpiHtml(title, value) {
-        return "<div class='title'>" + title + "</div><div class='value'>" + value + "</div>";
+    function append_kpi(row, tip, html) {
+        row.append("div").attr("class", "kpi").attr("title", "Peak " + tip + " usage last 10 days").html(html);
     }
 
-    var groups = d3.select("#Groups").selectAll("div.row").data(json).enter();
+    function append_kpi_space(row) {
+        row.append("div").attr("class", "kpi-space");
+    }
+
+    function kpi_usage(title, used, total) {
+        var percent = Math.round(used / total * 100);
+        var value = used + "/" + total;
+        return '<div class="sub-value">' + percent + '%</div>' +
+               '<div class="title">' + title + '</div>' +
+               '<div class="value">' + value + '</div>';
+    }
+
+    function kpi_io(title, read, write) {
+        return '<div class="title">' + title + '</div>' +
+               '<div class="value">' + write + '<span class="read">' + read + '</span></div>';
+    }
+
+
+    var groups = d3.select("#Groups").selectAll("div.row").data(json.groups).enter();
 
     var group = groups.append("div").attr("class", "row").attr("id", function(d){return d.group;});
     var kpis = group.append("div").attr("class", "col-md-3");
@@ -12,29 +30,15 @@ d3.json("dashboard.json", function(error, json) {
     kpis.append("div").attr("class", "swimlane").text(function(d) {return d.owner + ": " + d.name;});
 
     var row = kpis.append("div").attr("class", "kpi-row");
-
-    //appendKPI(row, tip, title, value);
-    //appendKpiSpace(row);
-    //appendKPI()
-
-    row.append("div").attr("class", "kpi").attr("title", "Peak usage last 10 days").html(function(d){
-        var title = Math.round(d.cpu.used / d.cpu.total * 100) + "% CPU";
-        var value = d.cpu.used + "/" + d.cpu.total;
-        return kpiHtml(title, value);
-    });
-    row.append("div").attr("class", "kpi-space");
-    row.append("div").attr("class", "kpi").html(function(d){
-        var title = Math.round(d.ram.used / d.ram.total * 100) + "% RAM [GiB]";
-        var value = d.ram.used + "/" + d.ram.total;
-        return kpiHtml(title, value);
-    });
+    append_kpi(row, "CPU", function(d){return kpi_usage("CPU", d.cpu.used, d.cpu.total);});
+    append_kpi_space(row);
+    append_kpi(row, "RAM [GiB]", function(d){return kpi_usage("RAM [GiB]", d.ram.used, d.ram.total);});
 
     row = kpis.append("div").attr("class", "kpi-row");
-    row.append("div").attr("class", "kpi").attr("title", "Disk").html(function(d){
-           var title = Math.round(d.cpu.used / d.cpu.total * 100) + "% CPU";
-           var value = d.cpu.used + "/" + d.cpu.total;
-           return kpiHtml(title, value);
-       });
+    append_kpi(row, "Disk IO [MB/s]", function(d) {return kpi_io('Disk IO [MB/s]', d.disk.read, d.disk.write)});
+    append_kpi_space(row);
+    append_kpi(row, "Net IO [MB/s]", function(d) {return kpi_io('Disk IO [MB/s]', d.net.read, d.net.write)});
+
 
 });
 
